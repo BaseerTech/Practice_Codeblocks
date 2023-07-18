c730ba354240eef782757d97a70a15779ee96d54
BaseerTech <139843064+BaseerTech@users.noreply.github.com>
2023-07-18 19:09:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2, product;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    product = num1 * num2;
+
+    cout << "The product of " << num1 << " and " << num2 << " is: " << product << endl;
+
+    return 0;
+}
